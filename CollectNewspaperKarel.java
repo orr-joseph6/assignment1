06f013881f5b477707fbd18e26ab4944510b4094
orr-joseph6 <orr.joseph6@yahoo.com>
2017-02-15 15:48:57 -0800

    taught karel how to collect newspaper and return to original position

--- CollectNewspaperKarel.java
+++ CollectNewspaperKarel.java
@@ -12,5 +12,25 @@ import stanford.karel.*;
 
 public class CollectNewspaperKarel extends SuperKarel {
 
+public void run(){
+	move();
+	move();
+	turnLeft();
+	turnLeft();
+	turnLeft();
+	move();
+	turnLeft();
+	move();
+	pickBeeper();
+	turnLeft();
+	turnLeft();
+	move();
+	move();
+	move();
+	turnLeft();
+	turnLeft();
+	turnLeft();
+	move();
+}
 	// You fill in this part
 }
